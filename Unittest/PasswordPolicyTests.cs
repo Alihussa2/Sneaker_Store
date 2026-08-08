a977ffe7abdae2d764d52c98c92eb48b11cbfd6c
hussain <alhu0002@stud.kea.dk>
2026-08-08 19:30:23 +0200

    CI test

--- Unittest/PasswordPolicyTests.cs
+++ Unittest/PasswordPolicyTests.cs
@@ -8,7 +8,7 @@ public class PasswordPolicyTests
 {
     // IKKE parametriseret: enkelt edge case (null-input)
     [Test]
-    public void ErGyldig_returns_false_when_password_is_null()
+    public void ErGyldig_returns_false_when_password_is_nul()
     {
         // Act
         var result = PasswordPolicy.ErGyldig(null);
